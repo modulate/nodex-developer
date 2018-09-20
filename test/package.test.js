13370ba71fdee62d23bdf23516bcda9e80f77701
Jared Hanson <jaredhanson@gmail.com>
2018-09-20 15:17:09 -0700

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,25 @@
 /* global describe, it */
 
-var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('modulate-develop-proj', function() {
+describe('@modulate/developer', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('io.modulate/developer');
+      
+      expect(json.assembly.components).to.have.length(1);
+      expect(json.assembly.components).to.include('project/main');
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
