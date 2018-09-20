63f12ee081d94416afdadd6392dcb52571330c72
Jared Hanson <jaredhanson@gmail.com>
2018-09-20 16:13:48 -0700

    Add comments.

--- app/project/main.js
+++ app/project/main.js
@@ -2,6 +2,18 @@ exports = module.exports = function(adapters) {
   
   var api = {};
   
+ /**
+  * Get software change management information.
+  *
+  * This function obtains information from software change management systems,
+  * including GitHub and Bitbucket.
+  *
+  * The vast majority of packages published to npm use GitHub as an SCM provider,
+  * but there are other providers in use including Bitbucket and GitLab, as well
+  * as others that tend to be common is specific regions such as China.  Tooling
+  * that is useful to analyze such information is available at:
+  *   - [all-the-package-repos](https://github.com/nice-registry/all-the-package-repos)
+  */
   api.info = function(url, options, cb) {
     if (typeof options == 'function') {
       cb = options;
